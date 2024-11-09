5c8d654eae77b9028a0b52ac37d90a2b185fb618
alfarj83 <148643005+alfarj83@users.noreply.github.com>
2024-11-09 15:28:44 -0500

    added index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import {
+    collection,
+    doc,
+    getDocs,
+    query,
+    setDoc,
+    deleteDoc,
+    getDoc,
+    getFirestore
+} from 'firebase/firestore';
+
+// create/add document
+
+
+// read document
+
+// update document
+
+// delete document
