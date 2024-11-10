679807c9b908085fc9622b1c11d9fb55b5a17461
alfarj83 <148643005+alfarj83@users.noreply.github.com>
2024-11-10 10:56:47 -0500

    tried fixing this but failed

--- doodycalls/src/components/AboutPage.js
+++ doodycalls/src/components/AboutPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './AboutPage.css';
+import kaitlinImage from "../assets/kaitlin.png"; // Adjust path as needed
+import jamieImage from "../assets/jamie.png";
 
 function AboutPage() {
   return (
     <div className="about-page">
       <header className="about-header">
-        <img src="team-image-placeholder.png" alt="Team" />
         <div>
           <h1>The Shitters!!!</h1>
           <p>ITWS/CS Majors</p>
@@ -15,7 +16,6 @@ function AboutPage() {
       <section className="team-section">
         {[...Array(4)].map((_, index) => (
           <div className="team-member" key={index}>
-            <div className="avatar">😊</div>
             <div className="member-info">
               <p><strong>Team Member {index + 1}</strong></p>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non justo vitae risus tempus consequat.</p>
